bbc8c796a620326506d2c1231fc9c4b91c6d13a3
nerfmiester <adrian.jackson@footballpools.com>
2012-08-30 15:02:20 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Casadelorto::Application.config.secret_token = 'aab46d1f4ae2716c312dbab212d9b7dd147381095beb6a7b490b918ef0635a3a60ac2d20f9165ec85457d6f4ccde112ccd8ba424e5f13c43584b9d1e8fed064b'
